75883775e7bd9ce1554af75bde82f61475a681e8
Deirdre Bringas <deirdrebringas@gmail.com>
2016-10-15 22:43:35 +0100

    dedededeedededede

--- levels.py
+++ levels.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 import json
 import numpy 
 import numpy.random as npr
@@ -6,47 +6,111 @@ import random as rm
 
 app = Flask(__name__)
 
+heights = {1:0.0, 2:0.1, 3:0.2, 4:0.3, 5:0.4, 6:0.5, 7:0.6, 8:0.7, 9:0.8, 10:0.9}
+
 score = 0
+deaths = 0
+highscore = 0
+deathScores = []
 
-def createBlueprint(l):
+seq = []
+seq_index = 0 
+
+propMatrix = [[0.1 for x in range(700)] for y in range(10)] 
+
+
+def checkLevel(v):
+	for i in range(2, len(v)-1):
+		if v[i-1]-v[i-2] > 0.3:
+			v[i-1] = v[i-2] + 0.3
+		elif v[i-2] == 0.0 and v[i-1] == 0.0 and v[i] == 0.0:
+			v[i] = heights[npr.random_integers(2,10)]
+ 		else:
+			v[i] = v[i]
+	return v
+
+
+def chooseNext(prev):
+	diceRoll = rm.random()
+	prev_col = [row[prev] for row in propMatrix]
+	if diceRoll < prev_col[0]:
+		return heights[1]
+	elif diceRoll < sum(prev_col[0:2]):
+		return heights[2]
+	elif diceRoll < sum(prev_col[0:3]):
+		return heights[3]
+	elif diceRoll < sum(prev_col[0:4]):
+		return heights[4]
+	elif diceRoll < sum(prev_col[0:5]):
+		return heights[5]
+	elif diceRoll < sum(prev_col[0:6]):
+		return heights[6]
+	elif diceRoll < sum(prev_col[0:7]):
+		return heights[7]
+	elif diceRoll < sum(prev_col[0:8]):
+		return heights[8]
+	elif diceRoll < sum(prev_col[0:9]):
+		return heights[9]
+	else:
+		return heights[10]
+
+def createBlueprint():
 	u = []
-	for i in range(0,l-1):
-		if len(u) > 2 and u[i-1] < 0.1 and u[i-2] < 0.1:
-			u.append(round(rm.uniform(0.1,1),3 ))
-		elif len(u) > 2 and u[i-1] > 0.1 and u[i-2] > 0.1:
-			u.append(round(rm.uniform(0, 0.11), 3))
-		else:
-			u.append(round(rm.random(), 3))
-	u.append(score)
-	return u
-
-def generateRand(n):
-	return round(rm.uniform(-n, n), 3)
+	u.append(heights[npr.random_integers(1,3)])
+	for i in range(1,700):
+		u.append(chooseNext(i-1))
+	return checkLevel(u)
 
 
-def createNewLevel(blueprint):
-	newLevel = []
-	n = rm.uniform(0.01, 0.09)
-	for element in blueprint[:-1]:
-		incr = generateRand(n)
-		temp = round(incr + element, 3)
-		if temp < 0:
-			newLevel.append(0)
-		elif temp >= 1:
-			newLevel.append(temp-1)
-		else:
-			newLevel.append(temp)
-	newLevel.append(0)
-	return newLevel
 
-test = createBlueprint(701)
+def changeProb(v):
+	p = 0.045 #temp
+	print score
+	for i in range(0, 10):
+		for j in range(score-2, score+2):
+			height = v[j]
+			if i == height*10:
+				propMatrix[i][j] = propMatrix[i][j] + p
+			propMatrix[i][j] = propMatrix[i][j] - 0.005
 
-data = {}
-data['vector'] = test
+
 
 @app.route('/')
 def index():
-	return jsonify(result=data)
+	global highscore
+	global deathScores
+	return render_template('index.html', highscore=highscore)
+
+@app.route('/level')
+def get_level():
+	vector = createBlueprint()
+	return jsonify(result=vector)
+
+@app.route('/post_level', methods=["GET", "POST"])
+def post_level():
+	vector = request.form["vector"]
+	global score 
+	score =  request.form["score"]
+	global highscore
+	potHighscore = request.form["highscore"]
+	global deathScores
+	deathScores.append(potHighscore)
+	if potHighscore > highscore:
+		highscore = potHighscore
+	changeProb(vector)
+	global deaths
+	deaths += 1
+	return "ok"
+
+@app.route('/scores')
+def deathsFun():
+	global highscore
+	global deathScores
+	ret = {"highscore": highscore, "deathScores": deathScores}
+	return jsonify(ret)
+
 
 if __name__ == '__main__':
-    app.run(debug=true,port=5001)
+    app.run(debug=True,port=5000)
+
+
